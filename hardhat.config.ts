9ded739efbe409dbaa902343b13a9def2d7ffe7a
Preston Ong <preston4896@me.com>
2023-12-05 16:52:56 +0800

    env update and hardhat script is working

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,11 @@
+import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+
+dotenv.config();
+
+const { SEPOLIA_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,6 +15,17 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200
       }
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: SEPOLIA_URL!
+      },
+      accounts: [{
+        privateKey: PRIVATE_KEY!,
+        balance: "10000000000000000000000" // 10000 ETH
+      }]
     }
   }
 };
