9d9df3c74cf93a4a033e35d26a4ab4a5fc9097a2
Preston Ong <preston4896@me.com>
2024-04-29 15:22:00 +0800

    refactored interface, ready for risc0

--- contracts/interfaces/IAttestation.sol
+++ contracts/interfaces/IAttestation.sol
@@ -1,6 +1,32 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+/**
+ * @title Interface standard that implement attestation contracts whose verification logic can be implemented
+ * both on-chain and with Risc0 ZK proofs
+ * @notice The interface simply provides two verification methods for a given attestation input.
+ * The user can either pay a possibly hefty gas cost to fully verify an attestation fully on-chain
+ * OR
+ * Provides ZK proofs from executing an off-chain program where the verification of such attestation is conducted.
+ * @dev should also implement Risc0 Guest Program to use this interface.
+ * See https://dev.risczero.com/api/blockchain-integration/bonsai-on-eth to learn more
+ */
 interface IAttestation {
-    function verifyAttestation(bytes calldata data) external returns (bool, uint256);
+    /**
+     * @notice full on-chain verification for an attestation
+     * @dev must further specify the structure of inputs/outputs, to be serialized and passed to this method
+     * @param input - serialized raw input as defined by the project
+     * @return output - the output upon completion of verification. The output data may require post-processing by the consumer
+     */
+    function verifyAndAttestOnChain(bytes calldata input) external returns (bytes memory output);
+
+    /**
+     * @notice inputs/outputs of this method is identical as { verifyAttestationOnChain }
+     * @param postStateDigest - A hash of the final memory state. Required to run the verifier, but
+     *     otherwise can be left unconstrained for most use cases.
+     * @param seal - The encoded cryptographic proof (i.e. SNARK).
+     */
+    function verifyAndAttestWithZKProof(bytes calldata input, bytes32 postStateDigest, bytes calldata seal)
+        external
+        returns (bytes memory output);
 }
--- contracts/v3/AutomataDcapV3Attestation.sol
+++ contracts/v3/AutomataDcapV3Attestation.sol
@@ -15,15 +15,6 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
     using BytesUtils for bytes;
     using LibString for bytes;
 
-    // TODO: invalid exit code definitions here...
-    uint8 constant INVALID_EXIT_CODE = 255;
-
-    bool private checkLocalEnclaveReport;
-    mapping(bytes32 enclave => bool trusted) private trustedUserMrEnclave;
-    mapping(bytes32 signer => bool trusted) private trustedUserMrSigner;
-
-    address public owner;
-
     constructor(
         address enclaveIdDaoAddr,
         address enclaveIdHelperAddr,
@@ -36,55 +27,47 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
         EnclaveIdBase(enclaveIdDaoAddr, enclaveIdHelperAddr)
         PEMCertChainBase(pckHelperAddr, crlHelperAddr, pcsDaoAddr)
         TCBInfoBase(tcbDaoAddr, tcbHelperAddr)
+    {}
+
+    error Failed_To_Verify_Quote();
+
+    function verifyAndAttestOnChain(bytes calldata input) external override returns (bytes memory output) {
+        bool verified;
+        (verified, output) = _verify(input);
+        if (!verified) {
+            revert Failed_To_Verify_Quote();
+        }
+    }
+
+    function verifyAndAttestWithZKProof(bytes calldata input, bytes32 postStateDigest, bytes calldata seal)
+        external
+        override
+        returns (bytes memory output)
     {
-        owner = msg.sender;
+        // TODO
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "onlyOwner");
-        _;
-    }
-
-    function setMrSigner(bytes32 _mrSigner, bool _trusted) external onlyOwner {
-        trustedUserMrSigner[_mrSigner] = _trusted;
-    }
-
-    function setMrEnclave(bytes32 _mrEnclave, bool _trusted) external onlyOwner {
-        trustedUserMrEnclave[_mrEnclave] = _trusted;
-    }
-
-    function toggleLocalReportCheck() external onlyOwner {
-        checkLocalEnclaveReport = !checkLocalEnclaveReport;
-    }
-
-    function verifyAttestation(bytes calldata data) external override returns (bool, uint256) {
-        return _verify(data);
-    }
-
-    /// --------------- validate parsed quote ---------------
-    function verifyParsedQuote(V3Struct.ParsedV3Quote calldata v3quote) external returns (bool, uint256) {
-        return _verifyParsedQuote(v3quote, bytes(""), V3Parser.packQEReport(v3quote.v3AuthData.pckSignedQeReport));
+    /**
+     * @dev may parse quotes off-chain, therefore slightly lowering gas cost as compared with { verifyAndAttestOnChain() }
+     */
+    function verifyParsedQuoteAndAttestOnChain(V3Struct.ParsedV3Quote calldata v3quote)
+        external
+        returns (bytes memory output)
+    {
+        bool verified;
+        (verified, output) =
+            _verifyParsedQuote(v3quote, bytes(""), V3Parser.packQEReport(v3quote.v3AuthData.pckSignedQeReport));
+        if (!verified) {
+            revert Failed_To_Verify_Quote();
+        }
     }
 
     /// @dev Provide the raw quote binary as input
-    /// @dev The return data of this method is constructed depending on the validity of the quote verification.
-    /// @dev After confirming that a quote has been verified, the attestation's validity then depends on the
-    /// status of the associated TCB.
-    /// @dev Example scenarios as below:
-    /// --------------------------------
-    /// @dev Invalid quote verification: returns (false, INVALID_EXIT_CODE)
-    ///
-    /// @dev For all valid quote verification, the validity of the attestation depends on the status of a
-    /// matching TCBInfo and this is defined in the _attestationTcbIsValid() method, which can be overwritten
-    /// in derived contracts. (Except for "Revoked" status, which also returns (false, INVALID_EXIT_CODE) value)
-    /// --------------------------------
-    /// @dev For all valid quote verification (and valid TCB), returns the following data:
-    /// (_attestationTcbIsValid(), 0)
-    /// @dev exitCode with varying TCB Statuses is defined in the {{ TCBInfoBase.TCBStatus }} enum
-    /// --------------------------------
     /// @dev view modifier omitted, because a PCCS cache miss emits an event
     /// @dev you can however, make a staticcall to this non-state changing method
-    function _verify(bytes calldata quote) private returns (bool, uint256) {
+    /// @return verified output: serialized as bytes of the following values:
+    ///     uint8 tcbStatus ++ bytes32 isvMrEnclave ++ bytes32 isvMrSigner ++ bytes64 isvReportData
+    function _verify(bytes calldata quote) private returns (bool verified, bytes memory output) {
         // Parse the quote input
         (
             bool successful,
@@ -93,7 +76,7 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
             bytes memory rawQeReport
         ) = V3Parser.parseInput(quote);
         if (!successful) {
-            return (false, INVALID_EXIT_CODE);
+            return (false, output);
         }
         return _verifyParsedQuote(parsedV3Quote, signedQuoteData, rawQeReport);
     }
@@ -105,25 +88,10 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
         V3Struct.ParsedV3Quote memory v3quote,
         bytes memory signedQuoteData,
         bytes memory rawQeReport
-    ) private returns (bool, uint256) {
-        uint256 exitCode = INVALID_EXIT_CODE;
-
-        // Step 0: Only validate the parsed quote if provided off-chain (gas-saving)
+    ) private returns (bool verified, bytes memory output) {
+        // Step 1: Only validate the parsed quote if provided off-chain (gas-saving)
         if (signedQuoteData.length == 0) {
             signedQuoteData = V3Parser.validateParsedInput(v3quote);
-        }
-
-        // Step 1: Verify application enclave report MRENCLAVE and MRSIGNER
-        {
-            if (checkLocalEnclaveReport) {
-                // 4k gas
-                bool mrEnclaveIsTrusted = trustedUserMrEnclave[v3quote.localEnclaveReport.mrEnclave];
-                bool mrSignerIsTrusted = trustedUserMrSigner[v3quote.localEnclaveReport.mrSigner];
-
-                if (!mrEnclaveIsTrusted || !mrSignerIsTrusted) {
-                    return (false, exitCode);
-                }
-            }
         }
 
         // Step 2: Verify enclave identity
@@ -140,10 +108,10 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
                 qeEnclaveReport.isvSvn
             );
             if (!verifiedEnclaveIdSuccessfully) {
-                return (false, exitCode);
+                return (false, output);
             }
             if (!verifiedEnclaveIdSuccessfully || qeTcbStatus == EnclaveIdTcbStatus.SGX_ENCLAVE_REPORT_ISVSVN_REVOKED) {
-                return (false, exitCode);
+                return (false, output);
             }
         }
 
@@ -178,7 +146,7 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
             bool tcbVerified;
             (tcbVerified, tcbStatus) = _checkTcbLevels(pckTcb, tcbInfoJson);
             if (!tcbVerified) {
-                return (false, exitCode);
+                return (false, output);
             }
         }
 
@@ -188,7 +156,7 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
         if (certification.certType == 5) {
             bool pckCertChainVerified = _verifyCertChain(parsedCerts);
             if (!pckCertChainVerified) {
-                return (false, exitCode);
+                return (false, output);
             }
         }
 
@@ -198,18 +166,18 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
                 parsedCerts[0].subjectPublicKey, signedQuoteData, v3quote.v3AuthData, rawQeReport
             );
             if (!enclaveReportSigsVerified) {
-                return (false, exitCode);
+                return (false, output);
             }
         }
 
-        exitCode = uint256(tcbStatus);
+        output = _serializeOutput(
+            tcbStatus,
+            v3quote.localEnclaveReport.mrEnclave,
+            v3quote.localEnclaveReport.mrSigner,
+            v3quote.localEnclaveReport.reportData
+        );
 
-        return (_attestationTcbIsValid(tcbStatus), exitCode);
-    }
-
-    function _attestationTcbIsValid(TCBStatus status) internal pure virtual returns (bool valid) {
-        return status == TCBStatus.OK || status == TCBStatus.TCB_SW_HARDENING_NEEDED
-            || status == TCBStatus.TCB_CONFIGURATION_AND_SW_HARDENING_NEEDED;
+        return (true, output);
     }
 
     function _enclaveReportSigVerification(
@@ -246,5 +214,15 @@ contract AutomataDcapV3Attestation is IAttestation, EnclaveIdBase, PEMCertChainB
             uint256(bytes32(key.substring(0, 32))),
             uint256(bytes32(key.substring(32, 32)))
         );
+    }
+
+    function _serializeOutput(
+        TCBStatus tcbStatus,
+        bytes32 isvMrEnclave,
+        bytes32 isvMrSigner,
+        bytes memory isvReportData
+    ) private pure returns (bytes memory serialized) {
+        require(isvReportData.length < 65, "invalid enclave report data length");
+        serialized = abi.encodePacked(tcbStatus, isvMrEnclave, isvMrSigner, isvReportData);
     }
 }
